e83760681e82b6e2806a2f738aa79c7b89f44f7e
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-06 21:25:32 -0500

    Obteniendo todo los productos del carrito

--- client/src/api/cart.js
+++ client/src/api/cart.js
@@ -15,3 +15,34 @@ export function getCartApi() {
   
     return JSON.parse(products);
   }
+
+  export async function getProductsCartApi() {
+    const idProducts = getCartApi();
+  
+    if (idProducts.lenght === 0) return null;
+  
+    try {
+      const products = [];
+  
+      for await (const idProduct of idProducts) {
+        const response = await fetch(`${API_URL}/products/${idProduct}`);
+        const result = await response.json();
+        products.push(result);
+      }
+  
+      const productsCount = countBy(products, (product) => {
+        return product.name;
+      });
+  
+      const combined = uniqBy(products, (product) => {
+        const productTemp = product;
+        productTemp.quantity = productsCount[product.name];
+        return productTemp.name;
+      });
+  
+      return combined;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
